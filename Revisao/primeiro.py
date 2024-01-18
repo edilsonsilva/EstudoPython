bb29641646dede4cb8e8972b42fa6762ef1b7725
Senac Tatuape <senac@localhost.localdomain>
2024-01-18 09:19:43 -0300

    Primeiros arquivos para a revisão

---
+++ Revisao/primeiro.py
@@ -0,0 +1,5 @@
+# Primeiro arquivo em Python para a revisão
+# declarando a variavel nome
+nome = ""
+nome = "Pedro Henrique"
+print(nome)
---
+++ Revisao/segundo.py
@@ -0,0 +1,6 @@
+# Este programa pede ao usuário entrar com o nome e o sobrenome
+nome = ""
+sobrenome = ""
+nome = input("Digite o seu nome: ")
+sobrenome = input("Digite o seu sobrenome")
+print("Olá sr(a). "+nome+" "+sobrenome)
