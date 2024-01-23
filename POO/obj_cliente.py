c6c021834325fa59abb91d648057982627684889
Senac Tatuape <senac@localhost.localdomain>
2024-01-23 08:31:12 -0300

    Trabalhando com hernça em python

---
+++ POO/obj_cliente.py
@@ -0,0 +1,9 @@
+from cliente import Cliente
+
+cliente_pedro = Cliente()
+cliente_pedro.dados("Pedro",23,"Masculino","pedro@uol.com.br")
+cliente_pedro.gravar()
+
+
+
+
---
+++ POO/obj_conta_corrente.py
@@ -0,0 +1,5 @@
+from class_conta_corrente import ContaCorrente as cc
+
+conta = cc()
+conta.abrirConta(35,12,"124548-10","Márcia",3500,1000)
+
